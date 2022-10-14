5aa94250d826ba5b0eb869c109d0fcfc345321e7
vanphuctxnd <112148433+vanphuctxnd@users.noreply.github.com>
2022-10-14 08:12:28 +0700

    Add files via upload

---
+++ baitap2/if-statemen.js
@@ -0,0 +1,8 @@
+var score = 75;
+var msg;
+if(score >= 50){
+    msg = 'Congratulations!';
+    msg += ' Proceed to the next round.';
+}
+var el = document.getElementById('answer');
+el.textContent = msg;
